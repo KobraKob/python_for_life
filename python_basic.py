ba807053a53e1ddd3184a38a5dbf6355c6714613
KobraKob <balavanthko@gmial.com>
2025-11-11 18:33:15 +0530

    basic

---
+++ python_basic.py
@@ -0,0 +1,22 @@
+#Creating variables 
+
+# name = 'Balavanth'
+# number = 8381
+# print(name, number)
+
+# a, b, c = 'apple', 'orange', 'pineapple'        
+# print(a, b, c)
+
+# x = "Python is good it seems"
+# print(x)
+
+# def some_functio():
+#     print(x)
+
+flower = "rose"
+def flower_functio():
+    flower = 'jasmine'
+    print('flower is ',flower)
+
+flower_functio()
+print("Flower is ",flower)
